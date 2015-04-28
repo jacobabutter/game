60019ea4b4eecc3f0e370c7983a7d61a46509eaa
jacob <jacobabutter@gmail.com>
2015-04-28 09:48:28 -0500

    gem
    added script for gems

---
+++ Random - Copy (2)/Assets/scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	static int gemCount;
+	static bool pick_up_1 = false;
+
+	// Use this for initialization
+	void Start () {
+		if (pick_up_1 == true) {
+			Destroy (GameObject.Find ("gem 1"));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag == "Gem" && collision.collider.name == "gem 1") {
+			gemCount++;
+			pick_up_1 = true;
+			Destroy (collision.gameObject, .0001f);
+			Debug.Log ("destroyed gem 1");
+		}
+	}
+}
